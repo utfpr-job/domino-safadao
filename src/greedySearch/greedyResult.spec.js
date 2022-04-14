15c2585950e87cb44ed3a030bb7d9fa9cd0ae06d
Luis Eduardo Oliveira <luis.eoliveira@vitta.me>
2022-04-14 19:58:12 -0300

    test: fix cases

--- src/greedySearch/greedyResult.spec.js
+++ src/greedySearch/greedyResult.spec.js
@@ -3,18 +3,10 @@ const { greedySearch }= require('./greedyResult')
 
 describe('greedySearch', () => {
   it('4.', () => {
-    const playerPieces = [
-      [ 2, 20 ],     [ 0.1, 5 ],
-      [ 0.25, 20 ],  [ 0.5, 200 ],
-      [ 0.05, 5 ],   [ 20, 0.25 ],
-      [ 0, 2 ],      [ 50, 200 ],
-      [ 200, 5 ],    [ 0.25, 0.5 ],
-      [ 0, 20 ],     [ 200, 0 ],
-      [ 0.5, 0.05 ]
-    ]
+    const player1Pieces = [[200, 1], [20, 5], [2, 100], [200, 50], [50, 10]]
+    const player2Pieces = [[20, 0], [20, 0.5], [50, 1], [200, 200], [50, 0.1]]
     const piecesObjective = [ 200, 0.25 ]
-    const nodes = mountGraph(playerPieces)(...piecesObjective)
-    console.log(JSON.stringify(nodes))
+    const nodes = mountGraph(player1Pieces)(player2Pieces)(piecesObjective)
     const result = greedySearch(nodes)
     console.log(result)
   })
