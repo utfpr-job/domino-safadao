f1dae09f83cdea68e628c4bedc67f79debcf4f5e
Luis Eduardo Oliveira <luis.eoliveira@vitta.me>
2022-04-14 19:58:06 -0300

    feat: receive 2 players pieces

--- src/greedySearch/mountGraph.js
+++ src/greedySearch/mountGraph.js
@@ -1,19 +1,19 @@
 const isSameArray = (arr1, arr2) => JSON.stringify(arr1) === JSON.stringify(arr2)
 
-const mountGraph = (playerPieces = [[0, 0]]) => (...valuesObjective) => {
+const mountGraph = (player1Pieces = [[0, 0]]) => (player2Pieces = [[0, 0]]) => (valuesObjective = [0]) => {
 
-  const mountNodeTree = (prevPieceSide) => (restPieces = [[0, 0]]) => {
-    const playablePieces = restPieces.filter((piece) => piece.includes(prevPieceSide))
+  const mountNodeTree = (prevPieceSide) => (actualPieces = [[0, 0]]) => (nextPieces = [[0, 0]]) => {
+    const playablePieces = actualPieces.filter((piece) => piece.includes(prevPieceSide))
 
     if (!playablePieces.length) return []
 
     return playablePieces.map((piece) => {
-      const anotherPieces = restPieces.filter((p) => !isSameArray(piece, p))
+      const anotherPieces = actualPieces.filter((p) => !isSameArray(piece, p))
       const notPlayableSide = piece[0] === prevPieceSide ? piece[1] : piece[0]
 
       return {
         piece,
-        nextNode: mountNodeTree(notPlayableSide)(anotherPieces)
+        nextNode: mountNodeTree(notPlayableSide)(nextPieces)(anotherPieces)
       }
     })
   }
@@ -21,7 +21,7 @@ const mountGraph = (playerPieces = [[0, 0]]) => (...valuesObjective) => {
   return valuesObjective.map((actualPiece) => {
     return {
       piece: [-1, actualPiece],
-      nextNode: mountNodeTree(actualPiece)(playerPieces)
+      nextNode: mountNodeTree(actualPiece)(player1Pieces)(player2Pieces)
     }
   })
 }
