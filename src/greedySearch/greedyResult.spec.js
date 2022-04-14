4a5968bc85a55cbc38192fd56ad3edbe81c1b7c6
Luis Eduardo Oliveira <luis.eoliveira@vitta.me>
2022-04-14 20:58:48 -0300

    test: add search tests

--- src/greedySearch/greedyResult.spec.js
+++ src/greedySearch/greedyResult.spec.js
@@ -6,7 +6,7 @@ describe('greedySearch', () => {
     const player1Pieces = [[200, 1], [20, 5], [2, 100], [200, 50], [50, 10]]
     const player2Pieces = [[20, 0], [20, 0.5], [50, 1], [200, 200], [50, 0.1]]
     const piecesObjective = [ 200, 0.25 ]
-    const nodes = mountGraph(player1Pieces)(player2Pieces)(piecesObjective)
+    const nodes = mountGraph()(player1Pieces)(player2Pieces)(piecesObjective)
     const result = greedySearch(nodes)
     console.log(result)
   })
--- src/mountGraph.spec.js
+++ src/mountGraph.spec.js
@@ -1,11 +1,13 @@
 const { mountGraph } = require('./mountGraph')
+const { search: greedySearch } = require('./greedySearch/search')
+const { search: aStar } = require('./aStar/search')
 
 describe('mountGraph', () => {
-  it('1.', () => {
+  it('1. two players with 2 pieces', () => {
     const player1Pieces = [[0, 0], [0, 1]]
     const player2Pieces = [[2, 0], [2, 1]]
     const piecesObjective = [0, 2]
-    const nodes = mountGraph(player1Pieces)(player2Pieces)(piecesObjective)
+    const nodes = mountGraph()(player1Pieces)(player2Pieces)(piecesObjective)
     expect(nodes).toEqual([
       {
         piece: [-1, 0],
@@ -36,11 +38,11 @@ describe('mountGraph', () => {
       },
     ])
   })
-  it('2.', () => {
+  it('2. two players with 5 pieces', () => {
     const player1Pieces = [[200, 1], [20, 5], [2, 100], [200, 50], [50, 10]]
     const player2Pieces = [[20, 0], [20, 0.5], [50, 1], [200, 200], [50, 0.1]]
     const piecesObjective = [200, 10]
-    const nodes = mountGraph(player1Pieces)(player2Pieces)(piecesObjective)
+    const nodes = mountGraph()(player1Pieces)(player2Pieces)(piecesObjective)
     expect(nodes).toEqual([
       {
         piece: [-1, 200],
@@ -132,5 +134,103 @@ describe('mountGraph', () => {
         ]
       }
     ])
+  })
+  describe('greedySearch', () => {
+    it('1. two players with 5 pieces', () => {
+      const player1Pieces = [[200, 1], [20, 5], [2, 100], [200, 50], [50, 10]]
+      const player2Pieces = [[20, 0], [20, 0.5], [50, 1], [200, 200], [50, 0.1]]
+      const piecesObjective = [200, 10]
+
+      const nodes = mountGraph(greedySearch)(player1Pieces)(player2Pieces)(piecesObjective)
+      expect(nodes).toEqual([
+        {
+          piece: [-1, 200],
+          nextNode: {
+            piece: [200, 50],
+            nextNode: {
+              piece: [50, 1],
+              nextNode: {
+                piece: [200, 1],
+                nextNode: {
+                  piece: [200, 200],
+                  nextNode: [],
+                },
+              },
+            },
+          },
+        },
+        {
+          piece: [-1, 10],
+          nextNode: {
+            piece: [50, 10],
+            nextNode: {
+              piece: [50, 1],
+              nextNode: {
+                piece: [200, 1],
+                nextNode: {
+                  piece: [200, 200],
+                  nextNode: {
+                    piece: [200, 50],
+                    nextNode: {
+                      piece: [50, 0.1],
+                      nextNode: [],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      ])
+    })
+  })
+  describe('aStar', () => {
+    it('1. two players with 5 pieces', () => {
+      const player1Pieces = [[200, 1], [20, 5], [2, 100], [200, 50], [50, 10]]
+      const player2Pieces = [[20, 0], [20, 0.5], [50, 1], [200, 200], [50, 0.1]]
+      const piecesObjective = [200, 10]
+
+      const nodes = mountGraph(aStar())(player1Pieces)(player2Pieces)(piecesObjective)
+      expect(nodes).toEqual([
+        {
+          piece: [-1, 200],
+          nextNode: {
+            piece: [200, 1],
+            nextNode: {
+              piece: [50, 1],
+              nextNode: {
+                piece: [200, 50],
+                nextNode: {
+                  piece: [200, 200],
+                  nextNode: [],
+                },
+              },
+            },
+          },
+        },
+        {
+          piece: [-1, 10],
+          nextNode: {
+            piece: [50, 10],
+            nextNode: {
+              piece: [50, 1],
+              nextNode: {
+                piece: [200, 1],
+                nextNode: {
+                  piece: [200, 200],
+                  nextNode: {
+                    piece: [200, 50],
+                    nextNode: {
+                      piece: [50, 0.1],
+                      nextNode: [],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      ])
+    })
   })
 })
